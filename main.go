04217c57f55fde1897d4499324d180dfad5721a5
Gavin Mogan <git@gavinmogan.com>
2019-04-30 00:25:33 -0700

    Many small fixes
    * Add support for external template file
    * Add support for outputting to stdout (-)
    
    Okay, two small changes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"io/ioutil"
 	"os"
+	"path"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -19,6 +20,7 @@ Check to see if there is an updated version available for installed charts.
 
 var outputFile string
 var inputFile string
+var templateFile string
 var version = "canary"
 var commit string
 var date string
@@ -43,6 +45,8 @@ type Charts struct {
 }
 
 func main() {
+	rootDir, _ := os.Getwd()
+
 	cmd := &cobra.Command{
 		Use:   "repo-html [flags]",
 		Short: fmt.Sprintf("Generates an html file for a repo (helm-repo-html %s)", version),
@@ -61,8 +65,9 @@ func main() {
 	}
 	cmd.AddCommand(versionCmd)
 
-	cmd.Flags().StringVarP(&outputFile, "output", "o", "index.html", "output filename")
-	cmd.Flags().StringVarP(&inputFile, "input", "i", "index.yaml", "input filename")
+	cmd.Flags().StringVarP(&outputFile, "output", "o", path.Join(rootDir, "index.html"), "output filename")
+	cmd.Flags().StringVarP(&inputFile, "input", "i", path.Join(rootDir, "index.yaml"), "input filename")
+	cmd.Flags().StringVarP(&templateFile, "template", "t", path.Join(rootDir, "index.tpl"), "template (go html/template format) filename")
 
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
@@ -71,6 +76,14 @@ func main() {
 
 func run(cmd *cobra.Command, args []string) error {
 	var err error
+	var htmlTemplate *template.Template
+	if _, err := os.Stat(templateFile); os.IsNotExist(err) {
+		log.Info("Template file not found, using default")
+		htmlTemplate = defaultHTMLTemplate
+	} else {
+		log.Info("Template file: " + templateFile)
+		htmlTemplate = template.Must(template.New(path.Base(templateFile)).ParseFiles(templateFile))
+	}
 
 	log.Info("Reading " + inputFile)
 	data, err := ioutil.ReadFile(inputFile)
@@ -84,13 +97,21 @@ func run(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return fmt.Errorf("Error processing yaml file: %v", err)
 	}
-	log.Info("Creating " + outputFile)
-	f, err := os.Create(outputFile)
-	if err != nil {
-		return fmt.Errorf("Error creating index file: %v", err)
+	var outputHandle *os.File
+
+	if outputFile == "-" {
+		outputHandle = os.Stdout
+		log.Info("Outputting template to stdout")
+	} else {
+		log.Info("Creating " + outputFile)
+		outputHandle, err = os.Create(outputFile)
+		if err != nil {
+			return fmt.Errorf("Error creating index file: %v", err)
+		}
+		log.Info("Outputting template " + outputFile)
 	}
-	log.Info("Outputting template " + outputFile)
-	err = htmlTemplate.Execute(f, charts)
+
+	err = htmlTemplate.Execute(outputHandle, charts)
 	if err != nil {
 		return fmt.Errorf("Error creating template: %v", err)
 	}
@@ -98,7 +119,7 @@ func run(cmd *cobra.Command, args []string) error {
 }
 
 var (
-	htmlTemplate = template.Must(template.New("htmlTemplate").Parse(`
+	defaultHTMLTemplate = template.Must(template.New("htmlTemplate").Parse(`
 <!DOCTYPE html>
 <html>
   <head>
@@ -114,14 +135,12 @@ var (
         margin: 0 auto;
         padding: 45px;
       }
-    
       @media (max-width: 767px) {
         .markdown-body {
           padding: 15px;
         }
       }
     </style>
-    
   </head>
 
   <body>
@@ -155,7 +174,6 @@ var (
 				</li>
 			{{end}}
       </ul>
-
     </section>
 		<time datetime="{{ .Generated.Format "2006-01-02T15:04:05" }}" pubdate id="generated">{{ .Generated.Format "Mon Jan 2 2006 03:04:05PM MST-07:00" }}</time>
   </body>
